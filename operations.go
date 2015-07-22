d4ea4c4c14bdfc47fd4b19fc88133ee01db8ca80
kentquirk <kentquirk@gmail.com>
2015-07-22 14:50:43 -0400

    Make it pass golint

--- operations.go
+++ operations.go
@@ -17,18 +17,18 @@ func (ss *StringSet) Clone() *StringSet {
 // abc & cde == c
 //
 // abc & !cde == ab
-func (ss1 *StringSet) intersection(ss2 *StringSet) *StringSet {
+func (ss *StringSet) intersection(ss2 *StringSet) *StringSet {
 	// if we have 2 positive sets we can optimize for length
 	if !ss2.IsNegative {
-		l1 := len(ss1.content)
+		l1 := len(ss.content)
 		l2 := len(ss2.content)
 		if l2 < l1 {
-			ss2, ss1 = ss1, ss2
+			ss2, ss = ss, ss2
 		}
 	}
 
 	intersection := New()
-	for k := range ss1.content {
+	for k := range ss.content {
 		if _, ok := ss2.content[k]; ok != ss2.IsNegative {
 			intersection.Add(k)
 		}
@@ -50,17 +50,17 @@ func (ss1 *StringSet) intersection(ss2 *StringSet) *StringSet {
 // !abc & cde == de
 //
 // !abc & !cde == !abcde
-func (ss1 *StringSet) Intersection(ss2 *StringSet) *StringSet {
+func (ss *StringSet) Intersection(ss2 *StringSet) *StringSet {
 	var r *StringSet
 	switch {
-	case !ss1.IsNegative && !ss2.IsNegative:
-		r = ss1.intersection(ss2)
-	case !ss1.IsNegative && ss2.IsNegative:
-		r = ss1.intersection(ss2)
-	case ss1.IsNegative && !ss2.IsNegative:
-		r = ss2.intersection(ss1)
-	case ss1.IsNegative && ss2.IsNegative:
-		r = ss1.Union(ss2).Negate()
+	case !ss.IsNegative && !ss2.IsNegative:
+		r = ss.intersection(ss2)
+	case !ss.IsNegative && ss2.IsNegative:
+		r = ss.intersection(ss2)
+	case ss.IsNegative && !ss2.IsNegative:
+		r = ss2.intersection(ss)
+	case ss.IsNegative && ss2.IsNegative:
+		r = ss.Union(ss2).Negate()
 	}
 	return r
 }
@@ -68,9 +68,9 @@ func (ss1 *StringSet) Intersection(ss2 *StringSet) *StringSet {
 // Union generates the union (OR) of the two sets.
 // Returns a new set and leaves the operands unchanged.
 // IsNegative has no effect.
-func (ss1 *StringSet) Union(ss2 *StringSet) *StringSet {
+func (ss *StringSet) Union(ss2 *StringSet) *StringSet {
 	union := New()
-	for k := range ss1.content {
+	for k := range ss.content {
 		union.Add(k)
 	}
 	for k := range ss2.content {
@@ -82,9 +82,9 @@ func (ss1 *StringSet) Union(ss2 *StringSet) *StringSet {
 // Difference is an assymetric set difference.
 // It subtracts the rhs from the lhs and returns a new set.
 // IsNegative has no effect.
-func (ss1 *StringSet) Difference(ss2 *StringSet) *StringSet {
+func (ss *StringSet) Difference(ss2 *StringSet) *StringSet {
 	difference := New()
-	for k := range ss1.content {
+	for k := range ss.content {
 		if _, ok := ss2.content[k]; !ok {
 			difference.Add(k)
 		}
@@ -94,15 +94,15 @@ func (ss1 *StringSet) Difference(ss2 *StringSet) *StringSet {
 
 // Equals checks whether two string sets have the same members.
 // IsNegative has no effect.
-func (ss1 *StringSet) Equals(ss2 *StringSet) bool {
-	if ss1.Length() != ss2.Length() {
+func (ss *StringSet) Equals(ss2 *StringSet) bool {
+	if ss.Length() != ss2.Length() {
 		return false
 	}
 	// if two sets have the same length then we know that if there is a difference,
 	// it must be the case that there are the same number of unique members
 	// in each set. So we can iterate over one set, and know that if every
 	// item was found, the two sets are equivalent
-	for k := range ss1.content {
+	for k := range ss.content {
 		if _, ok := ss2.content[k]; !ok {
 			return false
 		}
